33001b34a7cd9c7528e33fc6be731597a6e1bb7e
1334 <1334@shadowproject.net>
2018-11-08 20:13:48 +0100

    implement solution

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function findMedian (arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return false;
 
+  const arr = [...arr1,...arr2].sort();
+
+  if (arr.some(n => Number.isNaN(+n))) return false;
+  if (arr.length % 2 !== 0) return false;
+
+  return (arr[Math.floor((arr.length-1)/2)] + arr[Math.ceil((arr.length-1)/2)]) / 2.0
 }
-
 exports.findMedian = findMedian;
